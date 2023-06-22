a26a2f58e974d7b5e286b05016c9993ed4d68ac1
Frankmoises7 <frank.islas.zuniga@ciisa.cl>
2023-06-22 16:33:53 -0400

    fix cors por fin!!

--- src/index.js
+++ src/index.js
@@ -8,24 +8,17 @@ const { tallerRouter, authRouter } = require('./routes')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Configuración de CORS
+app.use(cors())
+
 app.use(express.json())
 
 app.use('/api/talleres', tallerRouter)
 app.use('/api/auth', authRouter)
 
-// Configuración de CORS
-app.use(cors())
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
-    app.use(cors())
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-      res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-      next();
-  });
     console.log('Connected to MongoDB')
     app.listen(PORT, () => {
       console.log(`Example app listening on port ${PORT}`)
--- src/models/Taller.js
+++ src/models/Taller.js
@@ -1,7 +1,42 @@
-/* 
-
 const mongoose = require('mongoose');
 
+const Schema = mongoose.Schema;
+
+const tallerSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  lon: {
+    type: Number,
+    required: true
+  },
+  direction: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  }
+});
+
+const Taller = mongoose.model('Taller', tallerSchema);
+
+module.exports = Taller;
+
+
+
+/* 
+
+======================  MODELO UTILIZANDO FORMATO GEOJSON  =========================
+
+
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tallerSchema = new Schema({
@@ -47,36 +82,7 @@ const Taller = mongoose.model('Taller', tallerSchema);
 module.exports = Taller; */
 
 
-const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-
-const tallerSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  lat: {
-    type: Number,
-    required: true
-  },
-  lon: {
-    type: Number,
-    required: true
-  },
-  direction: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  }
-});
-
-const Taller = mongoose.model('Taller', tallerSchema);
-
-module.exports = Taller;
 
 
 
